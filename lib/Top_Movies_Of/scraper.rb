e5f54c5893b9a48e7237573e12640e7c48b995e5
Parv Kumar <parvathikumar92@gmail.com>
2017-12-23 22:47:57 +0000

    added methods to Scraper class

--- lib/Top_Movies_Of/scraper.rb
+++ lib/Top_Movies_Of/scraper.rb
@@ -4,7 +4,20 @@ class TopMoviesOf::Scraper
     Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=#{input}"))
   end
 
+  def get_single_movie_page(movie_name)
+    split_name = movie_name.downcase.split
+    url_name = split_name.join("_")
+    
+    Nokogiri::HTML(open("https://www.rottentomatoes.com/m/#{url_name}"))
+  end
+
   def get_page_index(input)
     self.get_page(input).css(".table .unstyled article Link").text
   end 
+
+  def make_movies(input) 
+    get_page_index(input).each do |movie| 
+      TopMoviesOf::Restaurant.new(name = movie) #Make a movie class to create new instances per movie
+    end 
+  end
 end 
