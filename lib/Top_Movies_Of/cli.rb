89b9ad37ddc9df8b4cbf540f742f19bb2569cea5
Parv Kumar <parvathikumar92@gmail.com>
2017-12-23 21:48:20 +0000

    Added while loop to start method

--- lib/Top_Movies_Of/cli.rb
+++ lib/Top_Movies_Of/cli.rb
@@ -7,15 +7,24 @@ class TopMoviesOf::CLI
   end 
 
   def start 
-    puts ""
-    puts "What ranking movies would you like to see?"
-    puts "1. 1-10"
-    puts "2. 11-20"
-    puts "3. 21-30"
-    input = gets.strip.to_i 
-    show_movies(input)
+    answer = "y"
+    while answer.downcase == "y"
+      puts ""
+      puts "What ranking movies would you like to see?"
+      puts "1. 1-10"
+      puts "2. 11-20"
+      puts "3. 21-30"
+      input = gets.strip.to_i 
+      show_movies(input)
+
+
+      puts "Which movie would you like to see more information on?"
+      input = gets.strip.to_i
+
+      puts "Would you like to see more ranked movies? Type 'Y' for yes, or any other letter for no."
+      answer = gets.strip.to_s
+    end  
   end
-  
 
 
 end 
