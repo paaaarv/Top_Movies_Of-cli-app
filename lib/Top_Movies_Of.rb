98f875490d9293a78a8b4c098b2f538dcacc22a6
Parv Kumar <parvathikumar92@gmail.com>
2017-12-25 19:49:51 +0000

    added bin file, changed to executable

--- lib/Top_Movies_Of.rb
+++ lib/Top_Movies_Of.rb
@@ -1,4 +1,10 @@
-require "Top_Movies_Of/version"
+require_relative "Top_Movies_Of/version"
+require_relative './Top_Movies_Of/cli'
+require_relative './Top_Movies_Of/movie'
+require_relative './Top_Movies_Of/scraper'
+
+
+
 
 module TopMoviesOf
   # Your code goes here...
