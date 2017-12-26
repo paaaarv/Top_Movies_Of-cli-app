129cf460065ff31d7e0e49b103c3671eaad8508f
Parv Kumar <parvathikumar92@gmail.com>
2017-12-26 23:56:51 +0000

    added validations to CLI user input

--- lib/Top_Movies_Of/cli.rb
+++ lib/Top_Movies_Of/cli.rb
@@ -11,18 +11,43 @@ class TopMoviesOf::CLI
 
   def start
     answer = "y"
+    input = 4
     while answer.downcase == "y"
-      puts ""
-      puts "What ranking movies would you like to see?"
-      puts "1. 1-10"
-      puts "2. 11-20"
-      puts "3. 21-30"
-      input = gets.strip.to_i
-      show_movies(input)
-
-
+      while input > 3
+        puts ""
+        puts "What ranking movies would you like to see?"
+        puts "1. 1-10"
+        puts "2. 11-20"
+        puts "3. 21-30"
+        input = gets.strip.to_i
+        if input > 3
+          puts "Sorry, please enter 1,2, or 3 to see a list of top movies."
+        end
+      end
       puts "Which movie would you like to see more information on?"
-      input = gets.strip.to_i
+      single_mov = gets.strip.to_i
+      if input == 1
+        while single_mov <1 || single_mov > 10
+          puts "Sorry, please enter a number 1-10."
+          puts "Which movie would you like to see more information on?"
+          single_mov = gets.strip.to_i
+        end
+      #add method to see the single movie information
+    elsif input == 2
+      while single_mov <11 || single_mov > 20
+        puts "Sorry, please enter a number 1-10."
+        puts "Which movie would you like to see more information on?"
+        single_mov = gets.strip.to_i
+      end
+      #method to show movie
+    else
+      while single_mov <21 || single_mov > 30
+        puts "Sorry, please enter a number 1-10."
+        puts "Which movie would you like to see more information on?"
+        single_mov = gets.strip.to_i
+      end 
+      #method to show movie
+    end
 
       puts "Would you like to see more ranked movies? Type 'Y' for yes, or any other letter for no."
       answer = gets.strip.to_s
